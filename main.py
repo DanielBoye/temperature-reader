7e825d0da7ef739a169e4081606de13e8f79947a
Daniel Boye <83395536+DanielBoye@users.noreply.github.com>
2023-05-10 10:40:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from microbit import *
 
 data = []
 
-# Code in a 'while True:' loop repeats forever
+# Writes data 144 times to the array called data
 while len(data) < 144:
     data.append(temperature())
-    sleep(300)
+    # Sleeps in 5 minutes (in milliseconds)
+    sleep(300000) 
 
+# Write the 144 temperature readings in a temp.dat file
 with open('temp.dat', 'w') as min_fil:
     for i in data:
         min_fil.write(str(i))
